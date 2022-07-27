f8836727cb4ee8c4fbd86029d50aece8df28d95b
Hsu-Wei Jhih <gn00082077@gmail.com>
2022-07-27 10:40:33 +0800

    Add files via upload

---
+++ final/final.c
@@ -0,0 +1,31 @@
+﻿
+
+#include <stdio.h>
+#include<assert.h>
+int Rabbit(int t) {
+	if (t == 0) {
+		int intialR = 100;
+		return intialR;
+	}
+	return(4 * Rabbit(t - 1) - 2 * Fox(t - 1));
+}
+int Fox(int t) {
+	if (t == 0) {
+		int intialF = 100;
+		return intialF;
+	}
+	return(Rabbit(t - 1) + Fox(t - 1));
+}
+int main(void)
+{
+	int t;
+	printf("which year?\n");
+	scanf("%d", &t);
+	assert(t > 0 & t < 32);
+	printf("Rabbit = %d\n", Rabbit(t));
+	printf("Fox = %d\n", Fox(t));
+
+
+
+	return 0;
+}
---
+++ final2/final2.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include<assert.h>
+#define ARRAYSIZE 32
+
+void count(int t) {
+	int rArray[ARRAYSIZE];
+	int fArray[ARRAYSIZE];
+	rArray[0] = 100;
+	fArray[0] = 100;
+	for (int i = 1; i <= t; i++) {
+		rArray[i] = (4 * rArray[i-1] - 2 * fArray[i-1]);
+		fArray[i] = (rArray[i-1] + fArray[i-1]);
+	}
+	return printf("Rabbit = %d\nFox = %d\n",rArray[t], fArray[t]);
+}
+int main(void)
+{
+	int t;
+	printf("which year?\n");
+	scanf("%d", &t);
+	assert(t >= 0 & t < 32);
+	count(t);
+	return 0;
+}
+
+
